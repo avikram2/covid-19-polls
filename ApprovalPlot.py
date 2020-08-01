5c0a389ddcf03834b56fe94da0b03e7e8fd64d75
avikram2 <ayushvikram@gmail.com>
2020-08-01 10:27:20 -0400

    added code for infection concerns

--- ApprovalPlot.py
+++ ApprovalPlot.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
+
 # Code to generate plot of Trump COVID-19 Approval Rating based on 538's polling data
 
 new_df = pd.read_csv('./covid_approval_polls_adjusted.csv')
-sub_df = new_df[new_df['party']=='all']
+sub_df = new_df[new_df['party'] == 'all']
 print(sub_df)
 plt.plot(sub_df['startdate'], sub_df['approve_adjusted'], marker='o', linestyle='--', color='g', label='Approval')
 plt.xlabel('Polling Date [Ranges from 02/02/20 to 07/30/20]')
@@ -16,10 +17,10 @@ plt.tick_params(
     bottom=False,  # ticks along the bottom edge are off
     top=False,  # ticks along the top edge are off
     labelbottom=False)
-yticks = np.arange(0,110,10)
+yticks = np.arange(0, 110, 10)
 plt.yticks(yticks)
 # fontsize of the tick labels
 plt.legend()
-plt.text(2/2/20,90,"Data Visualization Created by Ayush Vikram, from FiveThirtyEight's Polling Data")
+plt.text(2 / 2 / 20, 90, "Data Visualization Created by Ayush Vikram, from FiveThirtyEight's Polling Data")
 plt.style.use('fivethirtyeight')
 plt.show()
--- InfectionConcernsChart.py
+++ InfectionConcernsChart.py
@@ -5,16 +5,16 @@ import numpy as np
 df = pd.read_csv('./covid_concern_toplines.csv')
 df = df[df['subject'] == 'concern-infected']
 df = df[df['party'] == 'all']
-df = df.iloc[::-1]
+df = df.iloc[::-1] #flip the dataframe as most recent data was originally at the top
 print(len(df))
 plt.plot(df['modeldate'], df['very_estimate'], label='Very Concerned')
 plt.plot(df['modeldate'], df['somewhat_estimate'], label='Somewhat Concerned')
 plt.plot(df['modeldate'], df['not_very_estimate'], label='Not Very Concerned')
 plt.plot(df['modeldate'], df['not_at_all_estimate'], label='Not At All Concerned')
 plt.legend()
-plt.yticks(np.arange(0,110,10))
+plt.yticks(np.arange(0, 110, 10))
 plt.xticks(np.arange(167, step=15))
-plt.text(7/29/20,90,'Visualization By Ayush Vikram; Data from FiveThirtyEight')
+plt.text(7 / 29 / 20, 90, 'Visualization By Ayush Vikram; Data from FiveThirtyEight')
 plt.title('How Concerned are Americans About Becoming Infected with COVID-19?')
 plt.xlabel('Date')
 plt.ylabel('%')
